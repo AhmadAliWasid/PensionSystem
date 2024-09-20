f9b9c3fab7bebb4b6b854fef4e81aeb957b4815b
Ali Ahmad <ahmadonline@outlook.com>
2024-09-20 15:26:44 +0500

    #improvement

---
+++ api/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PensionSystem.Entities.DTOs;
+using System;
+
+namespace WebAPI.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController(UserManager<IdentityUser> userManager,
+        ILogger<LoginModel> logger,
+        SignInManager<IdentityUser> signInManager) : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager = signInManager;
+        private readonly UserManager<IdentityUser> _userManager = userManager;
+        private readonly ILogger<LoginModel> _logger = logger;
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDTO loginModel)
+        {
+
+            // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+            var result = await _signInManager.PasswordSignInAsync(loginModel.Email,
+                loginModel.Password, false, false);
+            if (result.Succeeded)
+            {
+                var user = await _userManager.Users.Where(x => x.Email == loginModel.Email)
+                    .FirstOrDefaultAsync();
+                if (user != null)
+                {
+
+                }
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+
+}
