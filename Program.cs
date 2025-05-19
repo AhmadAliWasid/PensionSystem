5c87ac1abb03c3c7dbf9668ed00d571b1378bfde
Ali Ahmad <ahmadonline@outlook.com>
2025-05-19 21:19:32 +0500

    #improvements in proforma

--- Program.cs
+++ Program.cs
@@ -105,7 +105,7 @@ else
 {
     app.UseMigrationsEndPoint();
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+ 
     app.UseHsts();
 }
 // swagger
